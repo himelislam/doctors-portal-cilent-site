13b00283bd6916f5049154ffd02654f054899748
himelislam <himel7100@gmail.com>
2022-05-11 01:30:44 +0600

    appointment page added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Components/Pages/About/About';
+import Appointment from './Components/Pages/Appointment/Appointment';
 import Home from './Components/Pages/Home/Home';
 import Footer from './Components/Pages/Shared/Footer';
 import Navbar from './Components/Pages/Shared/Navbar';
 
 function App() {
   return (
-    <div>
+    <div className='max-w-7xl mx-auto'>
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='appointment' element={<Appointment></Appointment>}></Route>
         <Route path='about' element={<About></About>}></Route>
       </Routes>
       <Footer></Footer>
---
+++ src/Components/Pages/Home/ContactUs.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import bg from '../../../assets/images/bg.png'
+const ContactUs = () => {
+    return (
+        <section style={{
+            background : `url(${bg})`,
+            backgroundSize : 'cover' 
+        }}>
+            <form>
+            <input type="text" placeholder="Type here" class="input w-full max-w-xs" />
+            <input type="text" placeholder="Type here" class="input w-full max-w-xs" />
+            </form>
+        </section>
+    );
+};
+
+export default ContactUs;
